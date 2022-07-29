96497c39c5c018ffa3ee967abc85370e84cc01cf
Lloyd Stevens <stevens.lloyd@gmail.com>
2022-07-29 11:04:20 +0100

    second power

--- problems.js
+++ problems.js
@@ -1,16 +1,40 @@
 function oddIndices(arr) {
     // Return an array containing all the odd indices in the array
     // Your code here
+    let newArray = []
+    for (let i=1; i<arr.length; i+=2){
+         newArray.push(arr[i]);
+    }
+    return newArray
 }
 
 function oddReverse(arr) {
     // Return an array containing all the odd indices starting from the end
     // Your code here
+    let newArray = []
+    if (arr.length%2===0){
+        start = arr.length-1
+    } else {
+        start = arr.length-2
+    }
+
+    for (let i=start; i>0; i-=2){
+         newArray.push(arr[i]);
+    }
+    return newArray
 }
 
 function secondPower(arr) {
     // Return an array containing all indices that are powers of 2
     // Your code here
+    let newArray = [];
+    power = 0;
+    console.log(arr)
+    for (let i=1; i<arr.length; i = Math.pow(2, power)){
+        newArray.push(arr[i]);
+        power = power+1;
+    }
+    return newArray;
 }
 
 function nthPower(arr, n) {
